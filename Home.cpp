2c836619feac360e137484928dccd09b45022655
preetacc <loveynagi@gmail.com>
2013-04-15 00:30:59 -0700

    Update Home.cpp

--- Home.cpp
+++ Home.cpp
@@ -7,6 +7,13 @@ void main()
   textbackground(BLACK);
   textcolor(WHITE);
   clrscr();
-
+    gotoxy(37,5);
+      cout<<"MobiClo";
+    for(int i=0; i<80; i++)
+    {
+      gotoxy(i,20);
+        cout<<'._';
+      delay(100);
+    }
   getch();
 }
